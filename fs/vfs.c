e96b283ee6bb4e086382435ff74f77c6d767644a
chenyou-fdu <chenyou.fdu@gmail.com>
2017-02-18 18:09:10 +0800

    fix bugs in vfs header and finish the implementation

--- fs/vfs.c
+++ fs/vfs.c
@@ -13,6 +13,21 @@ uint32_t writeFS(fs_node_t *node, uint32_t offset, uint32_t size, uint8_t *buffe
 }
 
 void openFS(fs_node_t *node, uint8_t read, uint32_t write) {
-	if(node->open != 0) return node->open(node, read, write);
+	if(node->open != 0) node->open(node);
+}
+
+void closeFS(fs_node_t *node) {
+	if(node->close) node->close(node);
+}
+
+struct dirent* readdirFS(fs_node_t *node, uint32_t index) {
+	if((node->flags & 0x7) == FS_DIR && node->readdir != 0) 
+		return node->readdir(node, index);
+	else return 0;
+}
+
+fs_node_t* finddir_fs(fs_node_t *node, char *name) {
+	if( (node->flags & 0x7) == FS_DIR && node->finddir != 0) 
+		return node->finddir(node, name);
 	else return 0;
 }
--- include/vfs.h
+++ include/vfs.h
@@ -2,23 +2,32 @@
 #define VFS_H
 #include "types.h"
 
-#define FS_FILE 0x01
-#define FS_DIR  0x02
-#define FS_CHARDEVICE 0x03
-#define FS_BLOCKDEVICE 0x04
-#define FS_PIPE 0x05
-#define FS_SYMLINK 0x06
-#define FS_MOUNTPOINT 0x08
+// define file type
+#define FS_FILE 0x1
+#define FS_DIR  0x2
+#define FS_CHARDEVICE 0x3
+#define FS_BLOCKDEVICE 0x4
+#define FS_PIPE 0x5
+#define FS_SYMLINK 0x6
 
-typedef uint32_t (*read_type_t)(fs_node*, uint32_t, uint32_t, uint8_t);
-typedef uint32_t (*write_type_t)(fs_node*, uint32_t, uint32_t, uint8_t);
-typedef void (*open_type_t)(fs_node*);
-typedef void (*close_type_t)(fs_node*);
-typedef dirent* (*readdir_type_t)(fs_node*, uint32_t);
-typedef fs_node* (*finddir_type_t)(fs_node*, char* name);
+// is this dir a mount point
+#define FS_MOUNTPOINT 0x8
 
-typedef struct fs_node
-{
+struct fs_node;
+
+struct dirent {
+	char name[128];
+	uint32_t ino;
+};
+
+typedef uint32_t (*read_type_t)(struct fs_node*, uint32_t, uint32_t, uint8_t*);
+typedef uint32_t (*write_type_t)(struct fs_node*, uint32_t, uint32_t, uint8_t*);
+typedef void (*open_type_t)(struct fs_node*);
+typedef void (*close_type_t)(struct fs_node*);
+typedef struct dirent* (*readdir_type_t)(struct fs_node*, uint32_t);
+typedef struct fs_node* (*finddir_type_t)(struct fs_node*, char* name);
+
+typedef struct fs_node {
 	char name[128];
 	uint32_t flags;
 	uint32_t length;
@@ -26,19 +35,14 @@ typedef struct fs_node
 
 	read_type_t read;
 	write_type_t write;
-
+	open_type_t open;
+	close_type_t close;
 	readdir_type_t readdir;
 	finddir_type_t finddir;
 
-	fs_node *ptr;
+	struct fs_node *ptr;
 
 } fs_node_t;
-
-struct dirent
-{
-	char name[128];
-	uint32_t ino;
-};
 
 
 extern fs_node_t *fs_root;
@@ -47,7 +51,7 @@ uint32_t readFS(fs_node_t *node, uint32_t offset, uint32_t size, uint8_t *buffer
 uint32_t writeFS(fs_node_t *node, uint32_t offset, uint32_t size, uint8_t *buffer);
 void openFS(fs_node_t *node, uint8_t read, uint32_t write);
 void closeFS(fs_node_t *node);
-dirent* readdirFS(fs_node_t *node, uint32_t index);
+struct dirent* readdirFS(fs_node_t *node, uint32_t index);
 fs_node_t* finddir_fs(fs_node_t *node, char *name);
 
 #endif
