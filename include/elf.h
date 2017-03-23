45e09377f3d24d2d2e31dc3df2f34d3bc03fc400
chenyou-fdu <chenyou.fdu@gmail.com>
2017-03-23 22:12:09 +0800

    finish elf header

--- include/elf.h
+++ include/elf.h
@@ -1,6 +1,36 @@
 #ifndef ELF_H
 #define ELF_H
 
+#include "types.h"
 
+typedef struct elf_section_header {
+	uint32_t name;
+	uint32_t type;
+	uint32_t flags;
+	uint32_t addr;
+	uint32_t offset;
+	uint32_t size;
+	uint32_t link;
+	uint32_t info;
+	uint32_t addr_align;
+	uint32_t ent_size;
+} __attribute__((packed)) elf_section_header_t;
+
+typedef struct elf_symbol {
+	uint32_t name;
+	uint32_t value;
+	uint32_t size;
+	uint8_t info;
+	uint8_t other;
+	uint16_t shndx;
+} __attribute__((packed)) elf_symbol_t;
+
+typedef struct elf
+{
+	elf_symbol_t* symtab;
+	uint32_t symtabsz;
+	const char *strtab;
+	uint32_t strtabsz;
+} elf_t;
 
 #endif
